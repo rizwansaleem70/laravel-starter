0f26993d3ebfd043757aca38c50e53f255fe3d2f
Rizwan Saleem <rizwansaleem70@gmail.com>
2020-08-30 15:00:43 +0500

    added Sanctum package for api authentication

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
